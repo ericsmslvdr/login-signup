3f4679d4cc131ce027942c2ff544b0419428f128
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-07 20:23:43 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css//style.css">
+    <title>Login Signup</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="section">
+            <div class="formCard">
+                <h1>Login</h1>
+                <form action="" method="POST">
+                    <label class="">Username:</label>
+                    <br>
+                    <input type="text" name="username" id="username">
+                    <br>
+                    <label class="">Password:</label>
+                    <br>
+                    <input type="text" name="password" id="password">
+                    <br>
+                    <input type="submit" value="Login">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
