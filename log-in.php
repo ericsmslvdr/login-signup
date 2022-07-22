f463ae76aca97c426895a58bcac6d05f0a6f9024
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-22 11:40:25 +0800

    added bootstrap

---
+++ log-in.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="card p-5">
+        <h2 class="card-title">Login</h2>
+        <form action="" method="POST">
+            <label class="form-label mt-2">Username:</label>
+            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username Here" required>
+            <label class="form-label mt-2">Password:</label>
+            <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here" required>
+            <br>
+            <input type="submit" value="Login" class="btn btn-primary">
+            <a href="./index.php?isSignup=true"><button type="button" class="btn btn-outline-primary">Sign Up</button></a>
+        </form>
+    </div>
+</div>
+</body>
+
+</html>
--- sign-up.php
+++ sign-up.php
@@ -1,25 +1,23 @@
-<?php include('./components/header.php') ?>
-
-<body>
-    <div class="container">
-        <div class="card">
-            <h2 class="card-title">Sign Up</h2>
-            <form action="" method="POST">
-                <label class="form-label mt-2">Username:</label>
-                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username Here">
-                <label class="form-label mt-2">Password:</label>
-                <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here">
-                <label class="form-label mt-2">Repeat Password:</label>
-                <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here">
-                <label class="form-label mt-2">E-mail:</label>
-                <input type="text" name="email" id="email" class="form-control" placeholder="Enter Email Here">
-                <label class="form-label mt-2">Address:</label>
-                <input type="text" name="address" id="address" class="form-control" placeholder="Enter Address Here">
-                <input type="submit" class="btn btn-primary" value="Sign Up">
-                <a href="./index.php"><button type="button" class="btn btn-outline-primary">Login</button></a>
-            </form>
-        </div>
+<div class="container">
+    <div class="card p-5">
+        <h2 class="card-title">Sign Up</h2>
+        <form action="" method="POST">
+            <label class="form-label mt-2">Username:</label>
+            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username Here" required>
+            <label class="form-label mt-2">Password:</label>
+            <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here" required>
+            <!-- <label class="form-label mt-2">Repeat Password:</label>
+                <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here"> -->
+            <label class="form-label mt-2">E-mail:</label>
+            <input type="text" name="email" id="email" class="form-control" placeholder="Enter Email Here" required>
+            <label class="form-label mt-2">Address:</label>
+            <input type="text" name="address" id="address" class="form-control" placeholder="Enter Address Here" required>
+            <br>
+            <input type="submit" class="btn btn-primary" name="signup" value="Sign Up">
+            <a href="./index.php"><button type="button" class="btn btn-outline-primary">Login</button></a>
+        </form>
     </div>
+</div>
 </body>
 
 </html>
