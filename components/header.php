02606a5b33facbe84d0b87c59e1df45f2a9c1cb0
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-22 11:40:39 +0800

    include database php

--- components/header.php
+++ components/header.php
@@ -1,3 +1,5 @@
+<?php include('./scripts/database.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,5 +8,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Login</title>
+    <title>
+        <?php
+        // dynamic web title
+        echo $title = isset($_GET['isSignup']) ? 'SignUp' : 'Login';
+        ?>
+    </title>
 </head>
+
+<body>
+
+    <!-- navigation bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+        <div class="container">
+            <div class="collapse navbar-collapse">
+                <a class="navbar-brand" href="#">Login Signup Demo</a>
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="./index.php">Home</a>
+                </div>
+            </div>
+            <div class="d-flex">
+                <span class="text-light">tesdasd</span>
+            </div>
+        </div>
+    </nav>
