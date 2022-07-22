be6d05f7ad3ac3e5b39a684ffac5985664c048df
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-22 17:00:12 +0800

    added session, welcome title, greetings/logout btn

--- components/header.php
+++ components/header.php
@@ -1,5 +1,7 @@
-<?php include('./scripts/database.php') ?>
-
+<?php
+session_start();
+include('./scripts/database.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +13,11 @@
     <title>
         <?php
         // dynamic web title
-        echo $title = isset($_GET['isSignup']) ? 'SignUp' : 'Login';
+        echo $title = (isset($_GET['isSignup'])
+            ? 'SignUp'
+            : isset($_GET['isLoggedIn']))
+            ? 'Welcome'
+            : 'Login';
         ?>
     </title>
     <style>
@@ -32,8 +38,21 @@
                     <a class="nav-link active" href="./index.php">Home</a>
                 </div>
             </div>
-            <div class="d-flex">
-                <span class="text-light">tesdasd</span>
+            <div class="d-flex align-items-center">
+                <form method="POST">
+                    <?php
+                    if (isset($_SESSION['username'])) {
+                        echo '
+                            <span class="text-light mx-3">Hi there! ' . $_SESSION['username'] . '</span>
+                            <button type="submit" name="logout" class="btn btn-danger btn-sm">Logout</button>
+                        ';
+                    } else {
+                        echo '
+                            <span class="text-light mx-3">ericsslvdr</span>
+                        ';
+                    }
+                    ?>
+                </form>
             </div>
         </div>
     </nav>
