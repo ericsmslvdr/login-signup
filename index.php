a52d34c7c06427bd2c1221fce76e916f47ffdf10
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-22 11:40:12 +0800

    includes

--- index.php
+++ index.php
@@ -1,28 +1,12 @@
-<?php include('./components/header.php') ?>
+<?php
+// include header
+include('./components/header.php');
 
-<body>
-    <div class="container">
-        <div class="section">
-            <div class="formCard">
-                <div class="card-title">
-                    <h1>Login</h1>
-                    <hr>
-                </div>
-                <form action="" method="POST">
-                    <label class="text-form">Username:</label>
-                    <br>
-                    <input type="text" name="username" id="username" class="text-field" placeholder="Enter Username Here">
-                    <br>
-                    <label class="text-form">Password:</label>
-                    <br>
-                    <input type="text" name="password" id="password" class="text-field" placeholder="Enter Password Here">
-                    <br>
-                    <input type="submit" value="Login" class="login-btn">
-                    <a href="./signUp.php"><button type="button" class="login-btn">Sign Up</button></a>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
+// if condition is true include signup.php then skip
+if (isset($_GET['isSignup'])) {
+    include('./sign-up.php');
+    return;
+}
 
-</html>
+// if above is not satisfied, include login.php
+include('./log-in.php');
