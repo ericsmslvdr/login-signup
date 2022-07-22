6db720bb8947abe9f4f74e29619f4318e06e62c2
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-22 15:58:06 +0800

    improved ui

--- log-in.php
+++ log-in.php
@@ -1,15 +1,22 @@
-<div class="container">
-    <div class="card p-5">
-        <h2 class="card-title">Login</h2>
-        <form action="" method="POST">
-            <label class="form-label mt-2">Username:</label>
-            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username Here" required>
-            <label class="form-label mt-2">Password:</label>
-            <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here" required>
-            <br>
-            <input type="submit" value="Login" class="btn btn-primary">
-            <a href="./index.php?isSignup=true"><button type="button" class="btn btn-outline-primary">Sign Up</button></a>
-        </form>
+<div class="container d-flex">
+    <div class="card m-auto" style="width: 30em;">
+        <div class="card-header">
+            <h2 class="card-title" style="margin: 0;">Login</h2>
+        </div>
+        <div class="card-body p-2">
+            <form action="" method="POST">
+                <label class="form-label mt-2">Username:</label>
+                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username Here" required>
+                <label class="form-label mt-2">Password:</label>
+                <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here" required>
+                <br>
+                <input type="submit" value="Login" class="btn btn-primary">
+            </form>
+        </div>
+        <div class="card-footer d-flex justify-content-end align-items-center">
+            <p class="text-muted" style="margin: 0 1em;"><small>No Account Yet?</small></p>
+            <a href="./index.php?isSignup=true"><button type="button" class="btn btn-outline-primary btn-sm">Sign Up</button></a>
+        </div>
     </div>
 </div>
 </body>
--- sign-up.php
+++ sign-up.php
@@ -1,21 +1,28 @@
-<div class="container">
-    <div class="card p-5">
-        <h2 class="card-title">Sign Up</h2>
-        <form action="" method="POST">
-            <label class="form-label mt-2">Username:</label>
-            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username Here" required>
-            <label class="form-label mt-2">Password:</label>
-            <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here" required>
-            <!-- <label class="form-label mt-2">Repeat Password:</label>
+<div class="container d-flex">
+    <div class="card m-auto" style="width: 30em;">
+        <div class="card-header">
+            <h2 class="card-title" style="margin: 0;">Sign Up</h2>
+        </div>
+        <div class="card-body p-2">
+            <form action="" method="POST">
+                <label class="form-label mt-2">Username:</label>
+                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username Here" required>
+                <label class="form-label mt-2">Password:</label>
+                <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here" required>
+                <!-- <label class="form-label mt-2">Repeat Password:</label>
                 <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password Here"> -->
-            <label class="form-label mt-2">E-mail:</label>
-            <input type="text" name="email" id="email" class="form-control" placeholder="Enter Email Here" required>
-            <label class="form-label mt-2">Address:</label>
-            <input type="text" name="address" id="address" class="form-control" placeholder="Enter Address Here" required>
-            <br>
-            <input type="submit" class="btn btn-primary" name="signup" value="Sign Up">
-            <a href="./index.php"><button type="button" class="btn btn-outline-primary">Login</button></a>
-        </form>
+                <label class="form-label mt-2">E-mail:</label>
+                <input type="text" name="email" id="email" class="form-control" placeholder="Enter Email Here" required>
+                <label class="form-label mt-2">Address:</label>
+                <input type="text" name="address" id="address" class="form-control" placeholder="Enter Address Here" required>
+                <br>
+                <input type="submit" class="btn btn-primary" name="signup" value="Sign Up">
+            </form>
+        </div>
+        <div class="card-footer d-flex justify-content-end align-items-center">
+            <p class="text-muted" style="margin: 0 1em;"><small>Already Have an Account?</small></p>
+            <a href="./index.php"><button type="button" class="btn btn-outline-primary btn-sm">Login</button></a>
+        </div>
     </div>
 </div>
 </body>
