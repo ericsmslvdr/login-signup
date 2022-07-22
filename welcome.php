a353ce7efb3ceee914c60b10bda02d60247fa078
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-22 16:59:07 +0800

    first commit

--- welcome.php
+++ welcome.php
@@ -1,1 +1,22 @@
 <?php
+include('./components/header.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome <?= $_SESSION['username']; ?></title>
+</head>
+
+<body>
+    <div class="container d-flex">
+        <h1 class="m-auto">Welcome <?= $_SESSION['username']; ?></h1>
+    </div>
+</body>
+
+</html>
