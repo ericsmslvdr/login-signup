069665debb46752d0a9c36ff4df80e1ec5c3a925
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-23 21:58:03 +0800

    Update

--- index.php
+++ index.php
@@ -4,14 +4,15 @@ include('./components/header.php');
 
 // if logged in, immediately get back to welcome page when home is clicked
 if (isset($_SESSION['username'])) {
-    header('location: ./welcome.php');
+    include('./welcome.php');
+    exit();
 }
 
 if (isset($_GET['isSignup'])) {
-    // if condition is true include signup.php
+    // if condition is true show signup form
     include('./sign-up.php');
 } else {
-    // if above is not satisfied, include login.php
+    // if above is not satisfied, show login form
     include('./log-in.php');
 }
 
